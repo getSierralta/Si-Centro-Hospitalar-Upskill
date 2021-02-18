488348f79e53deab60c3f4bbbaa21430f98e37a9
getsierralta <sierraltamaximiliano8@gmail.com>
2021-02-18 10:02:55 +0000

    test

--- hospital-upskill/src/main/java/com/Bgrupo4/hospitalupskill/HospitalUpskillApplication.java
+++ hospital-upskill/src/main/java/com/Bgrupo4/hospitalupskill/HospitalUpskillApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HospitalUpskillApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test");
 		SpringApplication.run(HospitalUpskillApplication.class, args);
 	}
 
