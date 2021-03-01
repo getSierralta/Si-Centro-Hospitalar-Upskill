5686297c297503a92204ef2584e5cbabf88670c3
getsierralta <sierraltamaximiliano8@gmail.com>
2021-03-01 12:46:10 +0000

    lets see if this works

--- src/main/java/com/Bgrupo4/hospitalupskill/HospitalUpskillApplication.java
+++ src/main/java/com/Bgrupo4/hospitalupskill/HospitalUpskillApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HospitalUpskillApplication {
 
 	public static void main(String[] args) {
+		//Official branch
 		SpringApplication.run(HospitalUpskillApplication.class, args);
 	}
 
