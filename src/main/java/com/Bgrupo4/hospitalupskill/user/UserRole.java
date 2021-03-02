06104575737e0a5f91f5a3535c032e162133c9ee
vagamentedramatico <77270794+vagamentedramatico@users.noreply.github.com>
2021-03-02 20:39:33 +0000

    updated roles and added read/write permissions

--- src/main/java/com/Bgrupo4/hospitalupskill/user/UserRole.java
+++ src/main/java/com/Bgrupo4/hospitalupskill/user/UserRole.java
@@ -1,5 +1,27 @@
 package com.Bgrupo4.hospitalupskill.user;
 
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.Bgrupo4.hospitalupskill.user.UserPermission.*;
+
 public enum UserRole {
-    USER, ADMIN, DOCTOR, BOSS
+    // maybe remove USER and have AMDIN handle the tests
+    USER(Sets.newHashSet()),
+    UTENTE(Sets.newHashSet(UTENTE_WRITE)),
+    MEDICO(Sets.newHashSet(UTENTE_WRITE)),
+    COLABORADOR(Sets.newHashSet(UTENTE_WRITE, RESPONSAVEL_WRITE)),
+    RESPONSAVEL(Sets.newHashSet(MEDICO_WRITE, RESPONSAVEL_WRITE)),
+    ADMIN(Sets.newHashSet(COLABORADOR_WRITE, MEDICO_WRITE, UTENTE_WRITE, RESPONSAVEL_WRITE));
+
+    private final Set<UserPermission> permissions;
+
+    UserRole(Set<UserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<UserPermission> getPermissions() {
+        return permissions;
+    }
 }
