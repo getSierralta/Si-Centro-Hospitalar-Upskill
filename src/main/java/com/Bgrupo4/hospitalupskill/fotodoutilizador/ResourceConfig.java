99bbcf50185837d0d379c300b169b395846ee51d
j-040 <72324777+j-040@users.noreply.github.com>
2021-03-18 11:08:01 +0000

    foto do utilizador

--- src/main/java/com/Bgrupo4/hospitalupskill/fotodoutilizador/ResourceConfig.java
+++ src/main/java/com/Bgrupo4/hospitalupskill/fotodoutilizador/ResourceConfig.java
@@ -1,4 +1,4 @@
-package com.Bgrupo4.hospitalupskill.imagem;
+package com.Bgrupo4.hospitalupskill.fotodoutilizador;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
